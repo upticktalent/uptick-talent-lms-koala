18c0ad9a83d39a996d8ecaa7c1e4168315d5602d
odewale emmanuel <odebzemmygee@gmail.com>
2025-10-23 21:12:36 +0100

    test: login form test removed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import Box from '@/components/ui/box';
 import React from 'react';
+import LoginForm, { LoginFormData } from '@/components/common/login-form';
 
 const Entry = () => {
-  return <Box>page</Box>;
+  function handleSubmit(data: LoginFormData): void {
+    console.log(data);
+  }
+
+  return (
+    <Box className="flex min-h-svh sm: overflow-hidden bg-black">
+      <LoginForm role="student" onSubmit={handleSubmit} />
+    </Box>
+  );
 };
 
 export default Entry;
