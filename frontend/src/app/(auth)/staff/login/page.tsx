9a0125bab4e9c4296156e011e618e678f8291a08
Bello Qudus Folohunsho <qudusbello51@gmail.com>
2025-10-30 09:14:07 +0100

    refactor: replace <div> elements with MUI <Box> components

--- frontend/src/app/(auth)/staff/login/page.tsx
+++ frontend/src/app/(auth)/staff/login/page.tsx
@@ -3,23 +3,24 @@
 import Image from 'next/image';
 import { Mentor } from '../../../../../public/images';
 import LoginForm from '@/components/common/login-form';
+import Box from '@/components/ui/box';
 
 export default function LoginPage() {
   return (
-    <div className="flex h-screen bg-black relative justify-between overflow-hidden">
-      <div className="absolute inset-0 " style={{ backgroundImage: "url('/noise.svg')" }}></div>
+    <Box className="flex h-screen bg-black relative justify-between overflow-hidden">
+      <Box className="absolute inset-0 " style={{ backgroundImage: "url('/noise.svg')" }}></Box>
       {/* Left side */}
 
-      <div className="hidden lg:flex lg:w-1/2 max-w-2xl h-screen relative">
+      <Box className="hidden lg:flex lg:w-1/2 max-w-2xl h-screen relative">
         <Image src={Mentor} alt="mentor" fill className="rounded-r-[40px]" />
-      </div>
+      </Box>
 
       {/*Login form */}
-      <div className="w-full lg:w-1/2 flex items-center justify-center p-6 z-50">
-        <div className="w-full max-w-md">
+      <Box className="w-full lg:w-1/2 flex items-center justify-center p-6 z-50">
+        <Box className="w-full max-w-md">
           <LoginForm role="staff" onSubmit={() => {}} />
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 }
