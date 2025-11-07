3a05985257a9b4c559b3650e5ebf7892d8b136bd
Abdulkabir sultan <88564842+abdulkabirsultan@users.noreply.github.com>
2025-11-07 20:40:50 +0100

    Clean up unused imports in index.ts
    Removed unused imports from index.ts.

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -8,17 +8,6 @@ import { EmailTemplate, IEmailTemplate } from "./EmailTemplate.model";
 import { EmailLog } from "./EmailLog.model.";
 import { IEmailLog } from "./EmailLog.model.";
 import { EmailCampaign } from "./EmailCampaign.model";
-=======
-import { Application } from "./Application";
-import { Cohort } from "./Cohort";
-import { Track } from "./Track";
-import { User } from "./User";
-import { Assessment } from "./Assessment";
-import { EmailTemplate, IEmailTemplate } from "./EmailTemplate";
-import { EmailLog } from "./EmailLog";
-import { IEmailLog } from "./EmailLog";
-import { EmailCampaign } from "./EmailCampaign";
->>>>>>> main
 
 export {
   Application,
